d72430544a94dd077cd976547f5bc984a443fd4a
Federico Ficarelli <federico.ficarelli@gmail.com>
2018-01-30 14:50:21 +0100

    Issue demo is reproducible

--- packages/example-dependent/package.py
+++ packages/example-dependent/package.py
@@ -1,17 +1,17 @@
 from spack import *
+from spack.environment import EnvironmentModifications
+from llnl.util import filesystem as fs
 
 
 class ExampleDependent(Package):
-    homepage = "http://www.example.com"
-    url      = "http://www.example.com/example-1.2.3.tar.gz"
-
-    version('1.2.3', '0123456789abcdef0123456789abcdef')
+    url      = "http://www.bzip.org/1.0.6/bzip2-1.0.6.tar.gz"
+    version('1.0.6', '00b516f4704d4a7cb50a1d97e6e8e15b')
 
     depends_on('example-dependency')
 
     def install(self, spec, prefix):
-        make()
-        make('install')
-
-    def setup_dependent_environment(self, spack_env, run_env, dependent_spec):
-        pass
+        to_be_sourced = fs.join_path(prefix, 'SOURCE_ME')
+        with open(to_be_sourced, 'w') as f:
+            f.write('export SOURCE_ME_HAS_BEEN_SOURCED=1')
+        EnvironmentModifications.from_sourcing_file(to_be_sourced).apply_modifications()
+        fs.touch(fs.join_path(prefix, 'INSTALL_DONE'))
